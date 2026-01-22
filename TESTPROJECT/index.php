cedf80a09a0ffb40182500b30e164e990b1df3d2
Chong5099 <m-10472039@moe-dl.edu.my>
2026-01-22 08:53:21 +0800

    Add files via upload

---
+++ TESTPROJECT/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+    <meta charset="UTF-8">
+    <title>GOOTOK</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- 连接 CSS -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <!-- 顶部导航栏 -->
+    <div class="navbar">
+        <div class="logo">GOOTOK</div>
+        <div class="nav-buttons">
+            <button class="btn login">登录</button>
+            <button class="btn register">注册</button>
+        </div>
+    </div>
+
+    <!-- 主内容区 -->
+    <div class="container">
+        <div class="post-box">
+            <h2>欢迎来到 GOOTOK 👋</h2>
+            <p>一个属于你的社交平台</p>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="post-box">  
+                <p>阿巴阿巴</p>
+        </div>
+    </div>
+
+
+</body>
+</html>
+
